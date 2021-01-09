7413f7fb67d13440f737b4a9ea160e7cdb5493ec
Jim Nelson <jim@jimnelson.us>
2021-01-08 17:42:06 -0700

    cleanup

--- examples/GodotCiv3/Civ3Media.cs
+++ examples/GodotCiv3/Civ3Media.cs
@@ -1,6 +1,5 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 using ReadCivData.ConvertCiv3Media;
 
 public class Civ3Media : Node2D
@@ -26,16 +25,12 @@ public class Civ3Media : Node2D
     {
         string path = this.Civ3Path + "/Art/Terrain/xpgc.pcx";
         Pcx TerrainTexture = new Pcx(path);
-        GD.Print(TerrainTexture.Width);
-        GD.Print(TerrainTexture.Height);
         Image GrassLandImage = new Image();
         GrassLandImage.Create(TerrainTexture.Width, TerrainTexture.Height, false, Image.Format.Rgba8);
         GrassLandImage.Lock();
         for (int i = 0; i < TerrainTexture.Width * TerrainTexture.Height; i++)
         {
-            // try {
             GrassLandImage.SetPixel(i % TerrainTexture.Width, i / TerrainTexture.Width, Color.Color8(TerrainTexture.Palette[TerrainTexture.Image[i],0], TerrainTexture.Palette[TerrainTexture.Image[i],1], TerrainTexture.Palette[TerrainTexture.Image[i],2], TerrainTexture.Image[i] == 255 ? (byte)0 : (byte)255));
-            // } catch{ GD.Print(i % TerrainTexture.Width + " " + i / TerrainTexture.Width); }
         }
         GrassLandImage.Unlock();
         Sprite TerrSprite = new Sprite();
@@ -46,8 +41,6 @@ public class Civ3Media : Node2D
         TerrSprite.Vframes = 9;
         for (int i=0; i < 180; i++)
         {
-            // TerrSprite.Position.x = (i % 9) * 128 + (64 * ((i / 9) % 2));
-            // TerrSprite.Position.y = (i / 9) * 32;
             Vector2 pos = new Vector2((i % 9) * 128 + (64 * ((i / 9) % 2)),(i / 9) * 32 );
             TerrSprite.Position = pos;
             TerrSprite.Frame = (new Random()).Next() % 81;
