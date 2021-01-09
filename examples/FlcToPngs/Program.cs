3c2753e81300bf27a2e3ec8df8047f0d6b8366ee
Jim Nelson <jim@jimnelson.us>
2021-01-09 16:16:14 -0700

    adding to-png example stubs using ImageSharp Since I'm taking that ability out of the library

---
+++ examples/FlcToPngs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SixLabors.ImageSharp;
+
+namespace FlcToPngs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
