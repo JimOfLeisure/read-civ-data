bb0d2729f931472635fb952856fb8aa25bc23c0f
Jim Nelson <jim@jimnelson.us>
2021-01-10 15:45:04 -0700

    punted tilemap, still no tiles

--- examples/GodotCiv3/Civ3Media.cs
+++ examples/GodotCiv3/Civ3Media.cs
@@ -43,7 +43,40 @@ public class Civ3Media : Node2D
         }
     }
     public void TerrainAsTileMap() {
-        // Default values of square tiles and 64x64 size works for our needs
+        // Although tiles appear isometric, they are logically laid out as a checkerboard pattern on a square grid
+        TileMap TM = new TileMap();
+        TM.CellSize = new Vector2(64,32);
+        TileSet TS = new TileSet();
+        TM.TileSet = TS;
+
+        Pcx PcxTxtr = new Pcx(Civ3Path + "/Art/Terrain/xpgc.pcx");
+        Image ImgTxtr = ByteArrayToImage(PcxTxtr.Image, PcxTxtr.Palette, PcxTxtr.Width, PcxTxtr.Height);
+        ImageTexture Txtr = new ImageTexture();
+
+        int id = TS.GetLastUnusedTileId();
+        for (int y = 0; y < PcxTxtr.Height; y += 64) {
+            for (int x = 0; x < PcxTxtr.Width; x+= 128, id++) {
+                TS.CreateTile(id);
+                TS.TileSetTexture(id, Txtr);
+                TS.TileSetRegion(id, new Rect2(x, y, 128, 64));
+            }
+        }
+
+        int mywidth = 14;
+        for (int y = 0; y < mywidth; y++) {
+            for (int x = 0; x < mywidth; x+=2) {
+                try {
+                // TM.SetCellv(new Vector2(x, y), x + y * mywidth);
+                // TM.SetCellv(new Vector2(x + (y % 2), y), 0);
+                TM.SetCellv(new Vector2(x + (y % 2), y), (x / 2) % TS.GetTilesIds().Count);
+                } catch { GD.Print(x + y * mywidth); }
+            }
+        }
+        GD.Print(TS.GetTilesIds());
+        AddChild(TM);
+    }
+    // I'm failing to get the auto/atlas thing working in code
+    public void TerrainAsAtlasTileMap() {
         // Although tiles appear isometric, they are logically laid out as a checkerboard pattern on a square grid
         TileMap TM = new TileMap();
         TM.CellSize = new Vector2(64,32);
@@ -52,6 +85,7 @@ public class Civ3Media : Node2D
 
         int id = TS.GetLastUnusedTileId();
         TS.CreateTile(id);
+        TS.CreateTile(id+1);
 
         Pcx PcxTxtr = new Pcx(Civ3Path + "/Art/Terrain/xpgc.pcx");
         Image ImgTxtr = ByteArrayToImage(PcxTxtr.Image, PcxTxtr.Palette, PcxTxtr.Width, PcxTxtr.Height);
@@ -63,7 +97,10 @@ public class Civ3Media : Node2D
         TS.TileSetRegion(0, new Rect2(new Vector2(0,0), new Vector2(PcxTxtr.Width, PcxTxtr.Height)));
         TS.TileSetTileMode(0, TileSet.TileMode.AtlasTile);
         TS.AutotileSetSize(0, new Vector2(128,64));
-
+        TS.TileSetRegion(0, new Rect2(new Vector2(0,0), new Vector2(PcxTxtr.Width / 128 * 128, PcxTxtr.Height / 64 * 64)));
+        GD.Print(TS.TileGetRegion(0));
+        GD.Print(TS.AutotileGetSize(0));
+        // TS._ForwardAtlasSubtileSelection()
         // ResourceSaver.Save("tileset.tres", TS);
         // FIXME: None of the following seems to place tiles
         // TM.SetCellv(new Vector2(10,10), 0);
@@ -72,7 +109,8 @@ public class Civ3Media : Node2D
             for (int x = 0; x < mywidth; x+=2) {
                 try {
                 // TM.SetCellv(new Vector2(x, y), x + y * mywidth);
-                TM.SetCellv(new Vector2(x + (y % 2), y), 0);
+                // TM.SetCellv(new Vector2(x + (y % 2), y), 0);
+                TM.SetCellv(new Vector2(x + (y % 2), y), (x / 2) % 2);
                 } catch { GD.Print(x + y * mywidth); }
             }
         }
