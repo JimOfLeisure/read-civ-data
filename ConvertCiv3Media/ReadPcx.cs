ad864eb358a84991126e5b8262c262dc7ad976a9
Jim Nelson <jim@jimnelson.us>
2021-01-09 20:03:41 -0700

    cleanup, comments, readme

--- ConvertCiv3Media/ReadPcx.cs
+++ ConvertCiv3Media/ReadPcx.cs
@@ -1,19 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-/*
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.PixelFormats;
-*/
 
 namespace ReadCivData.ConvertCiv3Media
 {
-    // refactoring from static conversion to ImageSharp, to width, height, palette, and pixel data
     public class Pcx {
+
         public byte[,] Palette = new byte[256,3];
         public byte[] Image = new byte[]{};
         public int Width = 0;
         public int Height = 0;
+
+        // constructors
         public Pcx(){}
         public Pcx(string path) {
             this.Load(path);
@@ -22,6 +20,8 @@ namespace ReadCivData.ConvertCiv3Media
         // assumes 8-bit image with 256-color 8-bit rgb palette
         public void Load(string path) {
             byte[] PcxBytes = File.ReadAllBytes(path);
+
+            // Read info from PCX header
             int LeftMargin = BitConverter.ToInt16(PcxBytes, 4);
             int TopMargin = BitConverter.ToInt16(PcxBytes, 6);
             int RightMargin = BitConverter.ToInt16(PcxBytes, 8);
@@ -33,24 +33,31 @@ namespace ReadCivData.ConvertCiv3Media
             this.Width = RightMargin - LeftMargin;
             this.Height = BottomMargin - TopMargin;
             int ImageLength = BytesPerLine * Height;
+            // Palette is 256*3 bytes at end of file
             int PaletteOffset = PcxBytes.Length - 768;
 
+            // Populate color palette
             for (int i = 0; i < 256; i++) {
                 this.Palette[i,0] = PcxBytes[PaletteOffset + i * 3];
                 this.Palette[i,1] = PcxBytes[PaletteOffset + i * 3 + 1];
                 this.Palette[i,2] = PcxBytes[PaletteOffset + i * 3 + 2];
             }
+
+            // Populate image byte array
             List<byte> ListImage = new List<byte>();
+            // Encoding always have even number of bytes per line; if image width is odd, there is a junk byte in every row
             bool JunkByte = BytesPerLine > Width;
+            // Loop to decode run-length-encoded image data which begins at file offset 0x80
             for (int ImgIdx = 0, PcxIdx = 0x80, RunLen = 0, LineIdx = 0; ImgIdx < Width * Height; ) {
                 // if two most significant bits are 11
                 if ((PcxBytes[PcxIdx] & 0xc0) == 0xc0) {
                     // then it & 0x3f is the run length of the following byte
                     RunLen = PcxBytes[PcxIdx] & 0x3f;
                     PcxIdx++;
+                    // Repeat the pixel in the image RunLen times
                     for (int j = 0; j < RunLen; j++) {
+                        // Add pixel copy if it's not a junk byte
                         if (!(JunkByte && LineIdx % BytesPerLine == BytesPerLine - 1)) {
-                            // OutPixel[ImgIdx % Width, ImgIdx / Width] = Palette[PcxBytes[PcxIdx]];
                             ListImage.Add(PcxBytes[PcxIdx]);
                             ImgIdx++;
                         }
@@ -58,8 +65,8 @@ namespace ReadCivData.ConvertCiv3Media
                     }
                     PcxIdx++;
                 } else {
+                    // Add as literal pixel if it's not a junk byte
                     if (!(JunkByte && LineIdx % BytesPerLine == BytesPerLine - 1)) {
-                        // OutPixel[ImgIdx % Width, ImgIdx / Width] = Palette[PcxBytes[PcxIdx]];
                         ListImage.Add(PcxBytes[PcxIdx]);
                         ImgIdx++;
                     }
@@ -68,77 +75,6 @@ namespace ReadCivData.ConvertCiv3Media
                 }
             }
             this.Image = ListImage.ToArray();
-
         }
-        /*
-        // not a generalized pcx reader
-        // assumes 8-bit image with 256-color 8-bit rgb palette
-        static Image<Rgba32> Read(byte[] inPcx, int[] transparent) {
-            int LeftMargin = BitConverter.ToInt16(inPcx, 4);
-            int TopMargin = BitConverter.ToInt16(inPcx, 6);
-            int RightMargin = BitConverter.ToInt16(inPcx, 8);
-            int BottomMargin = BitConverter.ToInt16(inPcx, 10);
-            // assuming 1 color plane
-            // this is always even, so last byte may be junk if image width is odd
-            int BytesPerLine = BitConverter.ToInt16(inPcx, 0x42);
-
-            int Width = RightMargin - LeftMargin;
-            int Height = BottomMargin - TopMargin;
-            int ImageLength = BytesPerLine * Height;
-            int PaletteOffset = inPcx.Length - 768;
-
-            Rgba32[] Palette = new Rgba32[256];
-            // TODO: account for civ colors
-            //  for popheads I seemed to be using "if i < 16 or i % 2 == 0"
-            for (int i = 0; i < 256; i++) {
-                // Have to explicitly cast byte or it may use the normalized float constructor instead
-                Palette[i] = new Rgba32(
-                    (byte)inPcx[PaletteOffset + i * 3],
-                    (byte)inPcx[PaletteOffset + i * 3 + 1],
-                    (byte)inPcx[PaletteOffset + i * 3 + 2],
-                    Array.Exists<int>(transparent, e => e == i) ? (byte)0 : (byte)255
-                );
-            }
-            Image<Rgba32> OutPixel = new Image<Rgba32>(Width, Height);
-            bool JunkByte = BytesPerLine > Width;
-            for (int ImgIdx = 0, PcxIdx = 0x80, RunLen = 0, LineIdx = 0; ImgIdx < Width * Height; ) {
-                // if two most significant bits are 11
-                if ((inPcx[PcxIdx] & 0xc0) == 0xc0) {
-                    // then it & 0x3f is the run length of the following byte
-                    RunLen = inPcx[PcxIdx] & 0x3f;
-                    PcxIdx++;
-                    for (int j = 0; j < RunLen; j++) {
-                        if (!(JunkByte && LineIdx % BytesPerLine == BytesPerLine - 1)) {
-                            OutPixel[ImgIdx % Width, ImgIdx / Width] = Palette[inPcx[PcxIdx]];
-                            ImgIdx++;
-                        }
-                        LineIdx++;
-                    }
-                    PcxIdx++;
-                } else {
-                    if (!(JunkByte && LineIdx % BytesPerLine == BytesPerLine - 1)) {
-                        OutPixel[ImgIdx % Width, ImgIdx / Width] = Palette[inPcx[PcxIdx]];
-                        ImgIdx++;
-                    }
-                    PcxIdx++;
-                    LineIdx++;
-                }
-            }
-            return OutPixel;
-        }
-        // temp test function
-        public static void SaveOutPng() {
-            byte[] PcxData = File.ReadAllBytes(@"/Users/jim/code/src/tmp/Godot/civ3play/temp/popHeads-ORIG.pcx");
-            int[] Transparent = new int[]{254, 255};
-            Image<Rgba32> ImgData = Read(PcxData, Transparent);
-            string Path = "out.png";
-            if (File.Exists(Path)) {
-                File.Delete(Path);
-            }
-            using (FileStream fs = File.Create(Path)) {
-                ImgData.SaveAsPng(fs);
-            }
-        }
-        */
     }
 }
--- ConvertCiv3Media/readme.md
+++ ConvertCiv3Media/readme.md
@@ -14,9 +14,26 @@ The PCX reader works fairly well, but there is no accounting for unit civ colors
 
 The Flic reader mostly works but needs to better understsnd the Civ3-specific format changes and handle Civ3 color palettes properly.
 
+## Objects
+
+### Pcx
+
+Example: `Pcx MyImage = new Pcx(@"path/to/file.pcx");`
+
+### Properties
+
+- `int Width` - Width of the image
+- `int Height` - Height of the image
+- `byte[,] Palette` - A 256x3 byte array of 256 colors in red, green, blue order
+- `byte[] Image` - A flat byte array of the image data. Each byte is a pixel, the value of which is the index of the Palette indicating the color
+
+### Methods
+
+- Can pass a path string to the constructor and it will run Load() on creation
+- `void Load(string path)` - Loads and decodes the PCX file at the path location and populates the object properties
+
 ## Functions
 
-- Pcx.Read - static - Currently takes a byte array of an indexed 8-bit pcx file and returns SixLabors.ImageSharp.Image\<Rgba32\> of the image. Will probably change in the future, but currently works against Civ3 pcx files
 - Flic.Read - static - Currently takes a byte array of a Flic file and returns an SixLabors.ImageSharp.Image\<Rgba32\>[] of the converted image sequence. It works pretty fantastic on Civ3 units, and it works against other Civ3 Flics, but the colors are funny. Also I'm currently hard-coding unit shadow values which don't apply to non-unit Flics.
   - I have learned Civ3 Flics kind of do their own thing, especially with color palettes, so there is some work to do to get the colors right, get all frames, separate them into direction groups, and allow for changing the civ colors after conversion.
 
