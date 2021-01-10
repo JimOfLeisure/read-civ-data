570f0c456025dde5b63752699abe7457d926d3aa
Jim Nelson <jim@jimnelson.us>
2021-01-10 09:09:50 -0700

    refactored Image creation

--- examples/GodotCiv3/Civ3Media.cs
+++ examples/GodotCiv3/Civ3Media.cs
@@ -12,8 +12,8 @@ public class Civ3Media : Node2D
     {
         GD.Print(Civ3Path);
         GD.Randomize();
-        // this.TerrainAsSprites();
-        this.TerrainAsTileMap();
+        this.TerrainAsSprites();
+        // this.TerrainAsTileMap();
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,14 +26,8 @@ public class Civ3Media : Node2D
     {
         string path = this.Civ3Path + "/Art/Terrain/xpgc.pcx";
         Pcx TerrainTexture = new Pcx(path);
-        Image GrassLandImage = new Image();
-        GrassLandImage.Create(TerrainTexture.Width, TerrainTexture.Height, false, Image.Format.Rgba8);
-        GrassLandImage.Lock();
-        for (int i = 0; i < TerrainTexture.Width * TerrainTexture.Height; i++)
-        {
-            GrassLandImage.SetPixel(i % TerrainTexture.Width, i / TerrainTexture.Width, Color.Color8(TerrainTexture.Palette[TerrainTexture.Image[i],0], TerrainTexture.Palette[TerrainTexture.Image[i],1], TerrainTexture.Palette[TerrainTexture.Image[i],2], TerrainTexture.Image[i] == 255 ? (byte)0 : (byte)255));
-        }
-        GrassLandImage.Unlock();
+        // Image GrassLandImage = new Image();
+        Image GrassLandImage = ByteArrayToImage(TerrainTexture.Image, TerrainTexture.Palette, TerrainTexture.Width, TerrainTexture.Height);
         Sprite TerrSprite = new Sprite();
         ImageTexture TerrText = new ImageTexture();
         TerrText.CreateFromImage(GrassLandImage, 0);
@@ -55,5 +49,17 @@ public class Civ3Media : Node2D
         TileSet TS = new TileSet();
         TM.TileSet = TS;
         AddChild(TM);
+    }
+    Image ByteArrayToImage(byte[] ba, byte[,] palette, int width, int height) {
+        Image OutImage = new Image();
+        OutImage.Create(width, height, false, Image.Format.Rgba8);
+        OutImage.Lock();
+        for (int i = 0; i < width * height; i++)
+        {
+            OutImage.SetPixel(i % width, i / width, Color.Color8(palette[ba[i],0], palette[ba[i],1], palette[ba[i],2], ba[i] == 255 ? (byte)0 : (byte)255));
+        }
+        OutImage.Unlock();
+
+        return OutImage;
     }
 }
