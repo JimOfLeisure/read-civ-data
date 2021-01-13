85674acddbaed477d1e60d3f35d30753abfd9a07
Jim Nelson <jim@jimnelson.us>
2021-01-13 05:06:10 -0700

    tweak test

--- ConvertCiv3Media/test.ps1
+++ ConvertCiv3Media/test.ps1
@@ -6,7 +6,6 @@ $IniPath = $Civ3Root + '/Art/Units/warrior/Warrior.INI'
 
 Add-Type -Path ($NugetPath + '/ini-parser/3.4.0/lib/net20/INIFileParser.dll')
 
-
 $parser = New-Object IniParser.FileIniDataParser
 $data = $parser.ReadFile($IniPath)
-$data.Sections["Animations"] | Format-List
+$data.Sections["Animations"] | Select-Object KeyName, Value | Format-List
