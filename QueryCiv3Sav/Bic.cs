4c94416bb97da65851282239894c77f33609924c
Jim Nelson <jim@jimnelson.us>
2021-04-16 11:27:38 -0700

    ListSection scaffolding

--- QueryCiv3Sav/Bic.cs
+++ QueryCiv3Sav/Bic.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ReadCivData.QueryCiv3Sav
 {
@@ -9,11 +10,32 @@ namespace ReadCivData.QueryCiv3Sav
         {
             Bic = new Civ3File();
             Bic.Load(bicBytes);
+            Init();
         }
         public BicData(string bicPath)
         {
             Bic = new Civ3File();
             Bic.Load(bicPath);
+            Init();
+        }
+        public void Init()
+        {
+            //
+        }
+        public class ListSection<T>
+        {
+            int Offset;
+            int ItemCount;
+            public ListSection(Civ3File bic, int offset)
+            {
+                ItemCount = bic.ReadInt32(offset);
+                Offset = offset;
+            }
+            public List<T> Sections
+            { get {
+                List<T> OutList = new List<T>();
+                return OutList;
+            }}
         }
     }
 }
