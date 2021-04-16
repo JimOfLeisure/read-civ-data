e558b27971f1b30c9e39f388c76c390bcdf45816
Jim Nelson <jim@jimnelson.us>
2021-04-15 21:04:32 -0700

    refactoring, starting bic

--- QueryCiv3Sav/Bic.cs
+++ QueryCiv3Sav/Bic.cs
@@ -1,8 +1,15 @@
 using System;
 
-namespace ReadCivData.QueryCiv3Sav {
+namespace ReadCivData.QueryCiv3Sav
+{
     public class BicData
     {
-        //
+        public Civ3File Bic;
+        public BicData(string bicPath = "/Users/jim/civ3/Conquests/Conquests.biq")
+        {
+            // TODO: auto load default biq only if biq not in sav
+            Bic = new Civ3File();
+            Bic.Load(bicPath);
+        }
     }
 }
--- QueryCiv3Sav/Sav.cs
+++ QueryCiv3Sav/Sav.cs
@@ -1,50 +1,51 @@
 using System;
 
-namespace ReadCivData.QueryCiv3Sav {
+namespace ReadCivData.QueryCiv3Sav
+{
     public class SavData
     {
         public BicData Bic;
-        public Civ3File SavFile;
+        public Civ3File Sav;
         public GameSection Game;
         public WrldSection Wrld;
         public SavData(string path)
         {
-            SavFile = new Civ3File();
-            SavFile.Load(path);
-            Wrld = new WrldSection(SavFile);
-            Game = new GameSection(SavFile, Wrld.ContinentCount);
+            Sav = new Civ3File();
+            Sav.Load(path);
+            Wrld = new WrldSection(this);
+            Game = new GameSection(this);
         }
         public class GameSection
         {
             private int Offset;
-            private Civ3File SavFile;
-            public GameSection(Civ3File save, int continentCount)
+            private SavData Data;
+            public GameSection(SavData sav) //Civ3File save, int continentCount)
             // Need to know continent count for some offsets, not sure if it's somewhere in Game so grabbing it from Wrld
             {
+                Data = sav;
                 // TODO: Separate Sav and Bic data–or at least the pointers–in QueryCiv3
-                SavFile = save;
                 // TODO: Change nth to 1 after separating BIC and SAV data
-                Offset = SavFile.SectionOffset("GAME", 2);
+                Offset = Data.Sav.SectionOffset("GAME", 2);
             }
             // TODO: Return Bic sections, not IDs
-            public int DifficultyID{get{ return SavFile.ReadInt32(Offset+20); }}
-            public int UnitCount{get{ return SavFile.ReadInt32(Offset+28); }}
-            public int CityCount{get{ return SavFile.ReadInt32(Offset+32); }}
+            public int DifficultyID{get{ return Data.Sav.ReadInt32(Offset+20); }}
+            public int UnitCount{get{ return Data.Sav.ReadInt32(Offset+28); }}
+            public int CityCount{get{ return Data.Sav.ReadInt32(Offset+32); }}
         }
         public class WrldSection
         {
+            private SavData Data;
             private int Offset1, Offset2, Offset3;
-            private Civ3File SavFile;
-            public WrldSection(Civ3File save)
+            public WrldSection(SavData sav)
             {
-                SavFile = save;
-                Offset1 = SavFile.SectionOffset("WRLD", 1);
-                Offset2 = SavFile.SectionOffset("WRLD", 2);
-                Offset3 = SavFile.SectionOffset("WRLD", 3);
+                Data = sav;
+                Offset1 = Data.Sav.SectionOffset("WRLD", 1);
+                Offset2 = Data.Sav.SectionOffset("WRLD", 2);
+                Offset3 = Data.Sav.SectionOffset("WRLD", 3);
             }
-            public int ContinentCount{get{ return SavFile.ReadInt16(Offset1+4); }}
+            public int ContinentCount{get{ return Data.Sav.ReadInt16(Offset1+4); }}
             // TODO: Return Bic sections, not IDs
-            public int WsizID{get{ return SavFile.ReadInt32(Offset1+234); }}
+            public int WsizID{get{ return Data.Sav.ReadInt32(Offset1+234); }}
         }
     }
 }
