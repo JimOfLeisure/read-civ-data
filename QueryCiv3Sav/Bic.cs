e11c9befc34913c625035c15a96aa48d020a6a8f
Jim Nelson <jim@jimnelson.us>
2021-04-16 12:54:52 -0700

    broken checkpoint, changing direction

--- QueryCiv3Sav/Bic.cs
+++ QueryCiv3Sav/Bic.cs
@@ -24,18 +24,30 @@ namespace ReadCivData.QueryCiv3Sav
         }
         public class ListSection<T>
         {
+            Civ3File Bic;
             int Offset;
             int ItemCount;
             public ListSection(Civ3File bic, int offset)
             {
-                ItemCount = bic.ReadInt32(offset);
+                Bic = bic;
+                ItemCount = Bic.ReadInt32(offset);
                 Offset = offset;
             }
             public List<T> Sections
             { get {
+                int CurrentOffset = Offset + 4;
                 List<T> OutList = new List<T>();
+                for(int i=0; i<ItemCount; i++)
+                {
+                    int ItemLength = Bic.ReadInt32(CurrentOffset);
+                    T Item = new T();
+                    T.Init(Bic, CurrentOffset, ItemLength);
+                    CurrentOffset += ItemLength;
+
+                }
                 return OutList;
             }}
+            public void Init(Civ3File bic, int offset, int length){}
         }
     }
 }
