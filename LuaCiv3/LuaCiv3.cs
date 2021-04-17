95983d93ad353fc1ee2314ac44a4a54c782e30b6
Jim Nelson <jim@jimnelson.us>
2021-04-16 18:26:16 -0700

    stubbed all the bic sections

--- LuaCiv3/LuaCiv3.cs
+++ LuaCiv3/LuaCiv3.cs
@@ -18,6 +18,21 @@ namespace ReadCivData.LuaCiv3 {
             UserData.RegisterType<GameSection>();
             UserData.RegisterType<WrldSection>();
             UserData.RegisterType<BldgSection>();
+            UserData.RegisterType<CtznSection>();
+            UserData.RegisterType<CultSection>();
+            UserData.RegisterType<DiffSection>();
+            UserData.RegisterType<ErasSection>();
+            UserData.RegisterType<EspnSection>();
+            UserData.RegisterType<ExprSection>();
+            UserData.RegisterType<GoodSection>();
+            UserData.RegisterType<GovtSection>();
+            UserData.RegisterType<PrtoSection>();
+            UserData.RegisterType<RaceSection>();
+            UserData.RegisterType<TechSection>();
+            UserData.RegisterType<TfrmSection>();
+            UserData.RegisterType<TerrSection>();
+            UserData.RegisterType<WsizSection>();
+            UserData.RegisterType<FlavSection>();
         }
         private void SetGlobals()
         {
@@ -26,6 +41,21 @@ namespace ReadCivData.LuaCiv3 {
             Globals["game"] = SavFile.Game;
             Globals["wrld"] = SavFile.Wrld;
             Globals["bldg"] = SavFile.Bic.Bldg;
+            Globals["ctzn"] = SavFile.Bic.Ctzn;
+            Globals["cult"] = SavFile.Bic.Cult;
+            Globals["diff"] = SavFile.Bic.Diff;
+            Globals["eras"] = SavFile.Bic.Eras;
+            Globals["espn"] = SavFile.Bic.Espn;
+            Globals["expr"] = SavFile.Bic.Expr;
+            Globals["good"] = SavFile.Bic.Good;
+            Globals["govt"] = SavFile.Bic.Govt;
+            Globals["prto"] = SavFile.Bic.Prto;
+            Globals["race"] = SavFile.Bic.Race;
+            Globals["tech"] = SavFile.Bic.Tech;
+            Globals["tfrm"] = SavFile.Bic.Tfrm;
+            Globals["terr"] = SavFile.Bic.Terr;
+            Globals["wsiz"] = SavFile.Bic.Wsiz;
+            Globals["flav"] = SavFile.Bic.Flav;
         }
     }
 }
