1a1c341916d8002c338abc2ca152ebfb2920e21f
Jim Nelson <jim@jimnelson.us>
2021-10-23 13:00:16 -0700

    Adding examples/LuaHandlerPattern

---
+++ examples/LuaHandlerPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LuaHandlerPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // .NET 5 and later need Nuget package "System.Text.Encoding.CodePages" and registration for Windows-1252 (and other old locales) encoding
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+        }
+    }
+}
